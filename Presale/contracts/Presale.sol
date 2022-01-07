28455a74acfb79173c2e6786def9a9c164b2b101
Eskrano <pommmper@gmail.com>
2022-01-07 03:34:50 +0200

    WIP

--- Presale/contracts/Presale.sol
+++ Presale/contracts/Presale.sol
@@ -3,18 +3,60 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract Presale is Ownable {
-    struct Buyer {
-        address buyer;
-        uint256 tokensBought;
-        uint256 ethersSpent;
-        uint256 lastActive;
-        uint256 firstBuy;
-    }
+    using SafeMath for uint256;
+
+    event Bought (
+        address buyer,
+        uint256 amount
+    );
+
+     address private _token;
+
+     uint256 private _rate;
 
     constructor() {
         console.log(owner());
+    }
+
+    function setPresaleToken(address token_) public onlyOwner {
+        _token = token_;
+    }
+
+    function presaleToken() public view returns (address) {
+        return _token;
+    }
+
+    function setRate(uint256 rate) public onlyOwner {
+        _rate = rate;
+    }
+
+    function rate() public  view returns (uint256) {
+        return _rate;
+    }
+
+    function tokensLeft() public view returns (uint256) {
+        return IERC20(_token).balanceOf(address(this));
+    }
+
+    function buyTokens(uint256 payableAmount, address buyer) internal {
+        uint256 tokensToReceive = payableAmount.mul(_rate);
+
+        IERC20 tokenToSale = IERC20(_token);
+
+        require(tokensToReceive >= tokenToSale.balanceOf(address(this)),  "Presale::Not enough tokens to sale");
+
+        require(tokenToSale.transfer(buyer, tokensToReceive), "Presale::Failed to transfer tokens");
+
+        emit Bought(buyer, tokensToReceive);
+
+    }
+
+    receive() external payable {
+        buyTokens(msg.value, msg.sender);
     }
 
 
--- Presale/test/index.ts
+++ Presale/test/index.ts
@@ -1,23 +1,24 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { Presale } from "../typechain";
 
 describe("Presale.sol", function () {
 
   let deployer: SignerWithAddress;
+  let presale: Presale;
 
   beforeEach(async () => {
     const signers = await ethers.getSigners();
 
     deployer = signers[0];
+
+    const Presale = await ethers.getContractFactory("Presale");
+    presale = await Presale.deploy();
+    await presale.deployed();
   });
 
   it("Should be owned by the deployer", async function () {
-    const Presale = await ethers.getContractFactory("Presale");
-    const presale = await Presale.deploy();
-    await presale.deployed();
-
-
     expect(await presale.owner()).to.be.equal(deployer.address)
   });
 });
