4c3af7a8779d19d6703ff60f3c77f5ed9f507762
Eskrano <pommmper@gmail.com>
2022-01-11 09:11:59 +0200

    mainnet

--- Presale/frontend/src/config.ts
+++ Presale/frontend/src/config.ts
@@ -2,8 +2,8 @@ import { Config, Ropsten, Mainnet } from "@usedapp/core"
 
 export const commonConfig = {
     infuraKey: '254fc898c6c24be99475e8ec90ced016',
-    presaleContractAddress: '0x611eD75e0153d966b5A51C4fdEbc4971d5d0F051',
-    bashTokenAddress: '0x7629e1bebBA3b5a30bc4060846A2687F3A927715',
+    presaleContractAddress: '0xE63756E2FD565eE33dd3329Dd676c5b6D78a4feE',
+    bashTokenAddress: '0x56028c51945106a47ecd68258f794a5f9b98d9a6',
 }
 
 export const networksEth = {
@@ -14,14 +14,14 @@ export const networksEth = {
 export const walletConnectSettings =
 {
     rpc: {
-        1: `https://ropsten.infura.io/v3/${commonConfig.infuraKey}`,
+        1: `https://mainnet.infura.io/v3/${commonConfig.infuraKey}`,
         // 3: `https://ropsten.infura.io/v3/${commonConfig.infuraKey}`,
     },
     qrcode: true
 };
 
 export const config: Config = {
-    readOnlyChainId: Ropsten.chainId,
+    readOnlyChainId: Mainnet.chainId,
     readOnlyUrls: {
         [Ropsten.chainId]: 'https://ropsten.infura.io/v3/254fc898c6c24be99475e8ec90ced016',
         [Mainnet.chainId]: 'https://mainnet.infura.io/v3/254fc898c6c24be99475e8ec90ced016',
